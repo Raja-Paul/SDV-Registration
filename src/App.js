ee27f0f1f29d27c84f92893b875ab22ff0de7cac
Raja-Paul <101011683+Raja-Paul@users.noreply.github.com>
2023-01-03 16:19:28 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Client from './Client'
+import About from './About'
+import Service_cta from './Service_cta'
+import Faq from './Faq'
+import Contact from './Contact'
+import Footer from './Footer'
+import './index.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <Navbar/>
+    <Hero/>
+    <Client/>
+    <About/>
+    <Service_cta/>
+    <Faq/>
+    <Contact/>
+    <Footer/>
+    </>
   );
 }
 
